80f5d9773968e8abd70d67b9581709efec3d7f98
Abubakar <abubakarafzal444@gmail.com>
2022-01-25 22:39:07 +0500

    creating repo

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      Shopping cart will display here
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Checkout = () => {
+  return <div>Checkout bag here </div>;
+};
+
+export default Checkout;
